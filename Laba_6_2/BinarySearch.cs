46702362dacb2d016442d423d04666718f97e484
Andrey <andrey2002335@gmail.com>
2020-06-07 15:36:53 +0300

    Win

--- Laba_6_2/BinarySearch.cs
+++ Laba_6_2/BinarySearch.cs
@@ -10,19 +10,22 @@ namespace Laba_6_2
     {
         //sdkfgjidfgdf]og
         //sdfgdfgdfgdfg
-        public BinarySearch(int size)
+        public BinarySearch(int size, params T[] par )
         {
             myGenericArray = new T[size];
+            par.CopyTo(myGenericArray,0);
         }
-        public T[] myGenericArray;
-        static int Search(int left, int right, T[] array, T target)
+        public T[] myGenericArray { get; set; }
+        public  int Search(int left, int right, T[] array, T target)
         {
             if (right - left < 0) throw new Exception($"{target} не найден");
             var index = (right - left) / 2 + left;
+            //Console.WriteLine((right - left) / 2 + left);
             var item = array[index];
-            var c = item.ToString();
-            var compareResult = c.CompareTo(target.ToString());
-
+            var c =Convert.ToInt32( item);
+            //Console.WriteLine(c+" "+ target.ToString());
+            var compareResult = c.CompareTo(Convert.ToInt32(target));
+           // Console.WriteLine(compareResult);
             if (compareResult < 0)
                 return Search(index + 1, right, array, target);
             if (compareResult > 0)
--- Laba_6_2/Program.cs
+++ Laba_6_2/Program.cs
@@ -10,6 +10,25 @@ namespace Laba_6_2
     {
         static void Main(string[] args)
         {
+            BinarySearch<string> binarySearch = new BinarySearch<string>(7,"a","b","bb","g","d","n","aa");
+            BinarySearch<int> binarySearch2 = new BinarySearch<int>(8,2,15,32,56,78,90,123,162);
+           
+                Console.WriteLine("HONCHAR ANDII IP-91 var7 BINARY SEARCH");
+            int x = 0;string y;
+            while (true)
+            {
+                Console.WriteLine("Хотите искать, если нет, то введите нет?");
+                y = Console.ReadLine();
+                if (y.ToLower() == "нет") break;
+                Console.WriteLine("Что ищем?");
+              x=  int.Parse(Console.ReadLine());
+                Console.Write("Индекс вашего числа: ");
+                Console.WriteLine(binarySearch2.Search(0, 7, binarySearch2.myGenericArray, x)+1);
+                //Console.WriteLine(binarySearch.Search(0, 6, binarySearch.myGenericArray, "b"));
+               
+            }
+
+            Console.ReadLine();
         }
     }
 }
